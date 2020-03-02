ba15ea10bd323398b6241b32969ca04771d1311a
Aflynn50 <alastairflynn@hotmail.co.uk>
2020-03-02 21:51:06 +0000

    I think its working

--- runner/main_runner.py
+++ runner/main_runner.py
@@ -33,7 +33,7 @@ def gen_games(repetitions):
     r = requests.get("http://games.ggp.org/stanford/games/")
     game_list = list(map(lambda x : x.strip('"') ,r.text.strip('[').strip(']').split(',')))
     print(game_list)
-    for game in game_list[:3]:
+    for game in game_list:
         num_players = get_num_roles(game)
         server_runner = get_server(game,num_players)
         player_runners = [get_player(x) for x in range(num_players)]
